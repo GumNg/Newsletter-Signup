43289944dd147e7f554ed4e3e83e2c78a0b529e1
Gum Ng <jon9609@gmail.com>
2022-08-09 14:05:43 -0700

    created express app to listen on port 3000 and log status

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.listen(3000, function() {
+    console.log("Server is up and running on port 3000");
+})
