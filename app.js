8a5b3c4a5473695b1dbcb01270db955ff6e61525
Gum Ng <jon9609@gmail.com>
2022-08-09 19:24:55 -0700

    added missing + to sendFile

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname("/signup.html"));
+    res.sendFile(__dirname + ("/signup.html"));
 });
 
 app.listen(3000, function() {
