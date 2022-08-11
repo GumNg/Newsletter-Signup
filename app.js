d164c1e939afd88cb10b9e99e06051cb167f0567
Gum Ng <jon9609@gmail.com>
2022-08-10 19:23:15 -0700

    added comments

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post("/", function (req, res) {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
-
+  // object required for api
   const data = {
     members: [
       {
@@ -35,22 +35,22 @@ app.post("/", function (req, res) {
       },
     ],
   };
-
+  // parses object data into string in JSON format
   const jsonData = JSON.stringify(data);
 
+  //   url for api
   const url = "https://us12.api.mailchimp.com/3.0/lists/c2d3a63ec5";
 
   const options = {
     method: "POST",
     auth: "gum1:",
   };
-
   const request = https.request(url, options, function (response) {
-
-    if(response.statusCode === 200){
-        res.sendFile(__dirname + ("/success.html"));
+    // response to send user to page based on status code
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
     } else {
-        res.sendFile(__dirname + ("/failure.html"));
+      res.sendFile(__dirname + "/failure.html");
     }
 
     response.on("data", function (data) {
@@ -63,8 +63,8 @@ app.post("/", function (req, res) {
 });
 
 // post route failure to redirect to homepage
-app.post("/failure", function(req, res){
-    res.redirect("/");
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
